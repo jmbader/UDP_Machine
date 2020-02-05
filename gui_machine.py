80be6a4583e9f08bbc5350b4fd475aeb8b8ff0b0
jmbader0 <baderj7@gmail.com>
2020-02-04 21:27:34 -0500

    made more pretty and also made clear button

--- gui_machine.py
+++ gui_machine.py
@@ -24,36 +24,48 @@ class MyFirstGUI:
 
     def __init__(self, master):
         self.master = master
+
+        button_width = 20
+        button_height = 0
+        master.option_add("*Font", "-*-lucidatypewriter-medium-r-*-*-*-160-*-*-*-*-*-*")
+        master.configure(background='#92B2E5')
+
         master.title("GUI for sending UDP packets")
-        master.geometry('610x300')
+        master.geometry('600x685')
 
-        self.syn_button = Button(master, text="SYN", command=self.syn)
-        self.syn_button.grid(row=1, column=1, padx=5, pady=5, sticky=W)
+        self.syn_button = Button(master, text="SYN", command=self.syn, height=button_height, width=button_width)
+        self.syn_button.grid(row=1, column=1, padx=5, pady=5)
 
-        self.create_lcd_button = Button(master, text="Create a lcd at address 1 in package 1", command=self.create_lcd)
-        self.create_lcd_button.grid(row=2, column=1, columnspan=2, padx=5, pady=5, sticky=W)
+        self.create_lcd_button = Button(master, text="LCD ADR:1 PKG:1", command=self.create_lcd, height=button_height, width=button_width)
+        self.create_lcd_button.grid(row=2, column=1, columnspan=1, padx=5, pady=5)
 
-        self.fin_button = Button(master, text="FIN", command=self.fin)
-        self.fin_button.grid(row=3, column=1, padx=5, pady=5, sticky=W)
+        self.fin_button = Button(master, text="FIN", command=self.fin, height=button_height, width=button_width)
+        self.fin_button.grid(row=3, column=1, padx=5, pady=5)
 
-        self.set_eye_pos1_button = Button(master, text="Set eye at position 1", command=self.set_eye_pos_1)
-        self.set_eye_pos1_button.grid(row=4, column=1, padx=5, pady=5, sticky=W)
+        self.set_eye_pos1_button = Button(master, text="Set eye at position 1", command=self.set_eye_pos_1, height=button_height, width=button_width)
+        self.set_eye_pos1_button.grid(row=4, column=1, padx=5, pady=5)
 
-        self.set_eye_pos2_button = Button(master, text="Set eye at position 2", command=self.set_eye_pos_2)
-        self.set_eye_pos2_button.grid(row=4, column=2, padx=5, pady=5, sticky=W)
+        self.set_eye_pos2_button = Button(master, text="Set eye at position 2", command=self.set_eye_pos_2, height=button_height, width=button_width)
+        self.set_eye_pos2_button.grid(row=4, column=2, padx=5, pady=5)
 
-        self.set_eye_pos_button = Button(master, text="Set eye at random position", command=self.set_eye_pos)
-        self.set_eye_pos_button.grid(row=4, column=3, padx=5, pady=5, sticky=W)
+        self.set_eye_pos_button = Button(master, text="Set eye at rand position", command=self.set_eye_pos, height=button_height, width=button_width)
+        self.set_eye_pos_button.grid(row=4, column=3, padx=5, pady=5)
 
-        self.packet_input_text_box = Entry(master, width=100)
-        self.packet_input_text_box.grid(row=5, column=1, columnspan=4, padx=5, pady=5, sticky=W)
+        self.label_frame = LabelFrame(master, bg='#92B2E5', text="Send Raw Data")
+        self.label_frame.grid(row=5, column=1, columnspan=4, pady=20)
+
+        self.packet_input_text_box = Entry(self.label_frame, width=50)
+        self.packet_input_text_box.grid(row=5, column=1, columnspan=2, padx=5, pady=5)
         self.packet_input_text_box.bind('<Return>', func=self.packet_input_text)
 
-        self.text_output = Text(master, width=75)
-        self.text_output.grid(row=6, column=1, columnspan=4, padx=5, pady=5, sticky=W)
+        self.clear_button = Button(self.label_frame, text="Clear", width=13, command=self.clear)
+        self.clear_button.grid(row=5, column=3)
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.grid(row=0, column=1, padx=5, pady=5, sticky=W)
+        self.text_output = Text(self.label_frame, width=65)
+        self.text_output.grid(row=6, column=1, columnspan=4, padx=5, pady=5)
+
+        self.close_button = Button(master, text="Close", command=master.quit, height=button_height, width=button_width)
+        self.close_button.grid(row=1, column=3, padx=5, pady=5)
 
     def syn(self):
         print("SYN Packet")
@@ -123,6 +135,15 @@ class MyFirstGUI:
             self.print_response()
         except Exception:
             print("Input not a valid hex byte stream, try again chief")
+
+    def clear(self):
+        try:
+            self.sock.settimeout(0.1)
+            data, addr = self.sock.recvfrom(1024)  # buffer size is 1024 bytes
+        except Exception:
+            self.packet_input_text_box.insert('1.0', "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+            self.sock.settimeout(5)
+
 
 
 root = Tk()
